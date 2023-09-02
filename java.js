583eb4ea91baa60e2827e312eea1b265d5233c66
NusrathJahan22 <nusrathjahanjinat2002@gmail.com>
2023-09-02 20:15:38 +0600

    first commit

--- java.js
+++ java.js
@@ -22,20 +22,24 @@ const cardCategories = async (cardCategories) =>{
     const response= await fetch(`https://openapi.programming-hero.com/api/videos/category/${cardCategories}`);
     const data= await response.json();
     // console.log(data.data)
-
+    const noData=document.getElementById("no-content")
+    console.log(data.data.lenght)
+    if(data.data[0] == undefined){
+      noData.classList.remove('hidden');
+    }else{
+      noData.classList.add('hidden');
+    }
     const cardContainer= document.getElementById("card-container")
     cardContainer.innerHTML= '';
-    
+
     data.data.forEach((data) =>{
         console.log(data)
         const div= document.createElement("div")
         div.innerHTML= `
         <div class="card  bg-base-100 shadow-xl">
         <div>
-            <figure><img class="h-60 w-full overlay-text" src="${data.thumbnail}  alt="Shoes" /></figure>
-            <div class="overlay-text">
-            <p>${data?.others?.posted_date}</p>
-            </div>
+            <figure><img class="h-60 w-full overlay-text relative" src="${data.thumbnail}  alt="Shoes" /> <p class="absolute bg-red-100 p-1 rounded-lg">${data?.others?.posted_date}</p></figure>
+            
             <div class="card-body">
               <h2 class="card-title">
                 ${data.title}
@@ -55,7 +59,12 @@ const cardCategories = async (cardCategories) =>{
           </div>
         `;
         cardContainer.appendChild(div);
+
     })
+  
+
+
+
 }
 handleCategory();
-
+cardCategories(1000)
